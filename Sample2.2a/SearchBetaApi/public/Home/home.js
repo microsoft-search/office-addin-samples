1813ba010118f1ebdce9ad2edbe1e71da9b62431
Chris Givens <givenscj@Hotmail.com>
2019-10-22 21:18:19 -0700

    Update sample 2.2a for new endpoint.

--- Sample2.2a/SearchBetaApi/public/Home/home.js
+++ Sample2.2a/SearchBetaApi/public/Home/home.js
@@ -258,17 +258,18 @@
     function doSearch(query, entityType) {
   
           var request = {};
-          request.entityType = "microsoft.graph." + entityType;
+          request["entityTypes"] = [];
+          request["entityTypes"].push("microsoft.graph." + entityType);
           request.query = {};
           request.query["query_string"] = {};
           request.query["query_string"].query = query;
           request.from = 0;
           request.size = 25;
-          request["_sources"] = [];
-          request["_sources"].push("from");
-          request["_sources"].push("to");
-          request["_sources"].push("subject");
-          request["_sources"].push("body");
+          request["stored_fields"] = [];
+          request["stored_fields"].push("from");
+          request["stored_fields"].push("to");
+          request["stored_fields"].push("subject");
+          request["stored_fields"].push("body");
   
 
         var jsonObj = {};
@@ -277,7 +278,7 @@
         jsonObj["requests"] = requests;
 
         var jsonString = JSON.stringify(jsonObj);
-        var url = graphUrl + "/" + version + "/search";
+        var url = graphUrl + "/" + version + "/search/query";
 
         writeDebug(jsonString);
 
