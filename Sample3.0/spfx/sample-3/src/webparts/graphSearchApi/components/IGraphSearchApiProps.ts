99ed29127b93235145b1980bbda517b831b79c49
Chris Givens <givenscj@Hotmail.com>
2019-11-02 10:37:20 -0700

    breakout of spfx

--- Sample3.0/spfx/sample-3/src/webparts/graphSearchApi/components/IGraphSearchApiProps.ts
+++
@@ -1,8 +0,0 @@
-import { WebPartContext } from '@microsoft/sp-webpart-base';
-import { ClientMode } from './ClientMode';
-
-export interface IGraphSearchApiProps {
-  description: string;
-  clientMode: ClientMode;
-  context: WebPartContext;
-}
